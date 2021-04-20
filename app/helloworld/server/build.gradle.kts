28cb3c87ecc28c29dffc44af118e80a91a15fcaf
Christian Bay <c.bay@zbw.eu>
2021-04-20 17:12:01 +0200

    WIP

--- app/helloworld/server/build.gradle.kts
+++ app/helloworld/server/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     val kotlinVersion by System.getProperties()
     kotlin("jvm") version "$kotlinVersion"
     id("application")
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
 repositories {
@@ -11,6 +12,8 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":app:helloworld:api"))
+
     implementation("io.grpc:grpc-stub:1.37.0")
     implementation("io.grpc:grpc-protobuf:1.37.0")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
@@ -18,6 +21,8 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     implementation("org.slf4j:slf4j-api:1.7.30")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+    implementation("io.ktor:ktor-server-core:1.5.3")
+    implementation("io.ktor:ktor-server-netty:1.5.3")
 
     testImplementation(kotlin("test-testng"))
 }
@@ -31,5 +36,5 @@ tasks.withType<KotlinCompile>() {
 }
 
 application {
-    mainClass.set("de.zbw.server.helloworld.HelloWorldServer")
+    mainClass.set("de.zbw.api.helloworld.server.HelloWorldServer")
 }
--- app/helloworld/server/src/main/kotlin/de/zbw/api/helloworld/server/HelloWorldServer.kt
+++ app/helloworld/server/src/main/kotlin/de/zbw/api/helloworld/server/HelloWorldServer.kt
@@ -1,11 +1,12 @@
-package de.zbw.server.helloworld
+package de.zbw.api.helloworld.server
 
 import org.slf4j.LoggerFactory
 
 object HelloWorldServer {
     @JvmStatic
     fun main(args: Array<String>) {
-        LOG.info("Starting HelloWorldServer :)")
+        LOG.info("Starting the HelloWorldServer :)")
+        ServicePoolWithProbes(listOf()).start()
     }
 
     private val LOG = LoggerFactory.getLogger(HelloWorldServer::class.java)
