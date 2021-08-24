50faab3625d36c93b5fb104dabb89a9899354ff2
Christian Bay <christian.bay@posteo.net>
2021-08-24 15:13:53 +0200

    feat(ui-action): enum helper package

---
+++ app/access/server/ui/src/types/types.ts
@@ -0,0 +1,13 @@
+export enum License {
+  national = "Nationallizens",
+  konsortial = "Konsortiallizens",
+  pulish = "Publish-Komponente",
+  zbw = "ZBW-Nutzungsvereinbarungen",
+}
+
+export enum Publication {
+  essay = "Aufsatz",
+  journal = "Zeitschrift",
+  book = "Buch",
+  series = "Serie",
+}
