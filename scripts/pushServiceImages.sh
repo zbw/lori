b1852fb5cee82644eb4b18e326a3e2c6e0806656
Christian Bay <c.bay@zbw.eu>
2021-12-03 11:33:40 +0100

    build(STB3-143): debug outputs in gitlab pipeline

--- scripts/pushServiceImages.sh
+++ scripts/pushServiceImages.sh
@@ -5,6 +5,11 @@
 ##              that have been changed in this branch.
 set -x
 regex="app\/(\w*)\/(server|api)\/\S*"
+
+echo CI_COMMIT_REF_NAME=$CI_COMMIT_REF_NAME
+echo CI_COMMIT_BEFORE_SHA=$CI_COMMIT_BEFORE_SHA
+echo CI_COMMIT_BRANCH=$CI_COMMIT_BRANCH
+
 changedFiles=$(git diff-tree --no-commit-id --name-only -r origin/master -r $CI_COMMIT_SHA)
 changedServices=()
 for f in $changedFiles    # unquoted in order to allow the glob to expand
