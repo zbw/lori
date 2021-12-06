8675f863022a7f24c7bed8664ab72fd04ae2c292
Christian Bay <c.bay@zbw.eu>
2021-12-06 15:32:03 +0100

    build(STB3-143): fix broken pipe

--- scripts/pushServiceImages.sh
+++ scripts/pushServiceImages.sh
@@ -7,11 +7,9 @@ set -x
 regex="app\/(\w*)\/(server|api)\/\S*"
 MR_BRANCH_LAST_COMMIT_SHA=$(
       curl -s \
-                  --header "PRIVATE-TOKEN:$CI_JOB_TOKEN" \
-                          "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA" \
-                  | jq -r '.parent_ids | del(.[] | select(. == "'$CI_COMMIT_BEFORE_SHA'")) | .[-1]')
+                  --header "PRIVATE-TOKEN:$CI_JOB_TOKEN" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA" | jq -r '.parent_ids | del(.[] | select(. == "'$CI_COMMIT_BEFORE_SHA'")) | .[-1]')
 echo "MR_BRANCH_LAST_COMMIT_SHA: $MR_BRANCH_LAST_COMMIT_SHA"
-changedFiles=$(git diff-tree --no-commit-id --name-only -r $MR_BRANCH_LAST_COMMIT_SHA)
+changedFiles=$(git diff-tree --no-commit-id --name-only -r "$MR_BRANCH_LAST_COMMIT_SHA")
 changedServices=()
 for f in $changedFiles    # unquoted in order to allow the glob to expand
 do
@@ -23,7 +21,7 @@ done
 mapfile -t uniques < <(for v in "${changedServices[@]}"; do echo "$v";done| sort -u)
 for service in "${uniques[@]}";
 do
-	if ! ./gradlew :app:$service:server:jib
+    if ! ./gradlew :app:"$service":server:jib
     then exit 1
     fi
 done
