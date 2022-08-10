1eb4a2a96a58aea8c611e8fcf5e5154749afc74d
Christian Bay <c.bay@zbw.eu>
2022-08-10 15:20:09 +0200

    feat: introduce pinia
    Pinia is a Vue Store. See https://pinia.vuejs.org/ for more information.

--- app/lori/server/ui/src/App.vue
+++ app/lori/server/ui/src/App.vue
@@ -1,36 +1,74 @@
+<script lang="ts">
+import Vue from "vue";
+import { useHistoryStore } from "@/stores/history";
+
+export default Vue.extend({
+  name: "LoriApp",
+
+  setup() {
+    const historyStore = useHistoryStore();
+    const items = [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" },
+    ];
+
+    return {
+      items,
+      historyStore,
+    };
+  },
+});
+</script>
+
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Lori Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="@/assets/LogoLori.png"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
         />
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-chip
+            class="ma-2"
+            color="green"
+            text-color="white"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-avatar left class="green darken-4">
+              {{ historyStore.numberEntries }}
+            </v-avatar>
+            Änderungen
+          </v-chip>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in historyStore.history"
+            :key="index"
+          >
+            <v-list-item-action>
+              <v-btn fab small depressed color="primary">
+                {{ index }}
+              </v-btn>
+            </v-list-item-action>
+            <v-list-item-title
+              >{{ item.type.toString() }}: Right-Id
+              {{ item.rightId }}</v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
@@ -38,15 +76,3 @@
     </v-main>
   </v-app>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-
-export default Vue.extend({
-  name: "App",
-
-  data: () => ({
-    //
-  }),
-});
-</script>
