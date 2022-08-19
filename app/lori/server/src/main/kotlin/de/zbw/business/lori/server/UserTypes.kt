3f48170ae4f07b2a489116acc1fb91919cf07ae9
Christian Bay <c.bay@zbw.eu>
2022-08-19 13:09:44 +0200

    fix: typos

--- app/lori/server/src/main/kotlin/de/zbw/business/lori/server/UserTypes.kt
+++ app/lori/server/src/main/kotlin/de/zbw/business/lori/server/UserTypes.kt
@@ -6,7 +6,7 @@ package de.zbw.business.lori.server
  * Created on 08-17-2022.
  * @author Christian Bay (c.bay@zbw.eu)
  */
-enum class UserRole(val dbRepresentation: String){
+enum class UserRole(val dbRepresentation: String) {
     READ_ONLY("READONLY"),
     READ_WRITE("READWRITE"),
     ADMIN("ADMIN"),
--- app/lori/server/src/test/kotlin/de/zbw/business/lori/server/LoriServerBackendTest.kt
+++ app/lori/server/src/test/kotlin/de/zbw/business/lori/server/LoriServerBackendTest.kt
@@ -153,7 +153,7 @@ class LoriServerBackendTest : DatabaseTest() {
     }
 
     @Test
-    fun testHashString(){
+    fun testHashString() {
         val hashedPassword = backend.hashString("SHA-256", "foobar")
         val expectedHash = "c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2"
         assertThat(hashedPassword, `is`(expectedHash))
