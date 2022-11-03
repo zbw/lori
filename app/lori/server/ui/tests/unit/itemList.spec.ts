5eeae6a275175f0fba730c1528bad472e97aedfc
Christian Bay <c.bay@zbw.eu>
2022-11-03 11:51:46 +0100

    feat: implement tests in UI

--- app/lori/server/ui/tests/unit/itemList.spec.ts
+++ app/lori/server/ui/tests/unit/itemList.spec.ts
@@ -138,4 +138,304 @@ describe("Test ItemList UI", () => {
     await wrapper.vm.$nextTick();
     expect((wrapper.vm as any).getAlertLoad().value).toBeTruthy();
   });
+
+  it("testPublicationDateFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+    const searchStore = useSearchStore();
+
+    // when + then
+    searchStore.publicationDateFrom = "";
+    expect((wrapper.vm as any).buildPublicationDateFilter()).toBeUndefined();
+
+    // when + then
+    searchStore.publicationDateFrom = "2022";
+    searchStore.publicationDateTo = "";
+    expect((wrapper.vm as any).buildPublicationDateFilter()).toBe("2022-");
+
+    // when + then
+    searchStore.publicationDateFrom = "";
+    searchStore.publicationDateTo = "2023";
+    expect((wrapper.vm as any).buildPublicationDateFilter()).toBe("-2023");
+
+    // when + then
+    searchStore.publicationDateFrom = "2022";
+    searchStore.publicationDateTo = "2023";
+    expect((wrapper.vm as any).buildPublicationDateFilter()).toBe("2022-2023");
+  });
+
+  it("testPublicationTypeFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+    const searchStore = useSearchStore();
+    // when + then
+    expect((wrapper.vm as any).buildPublicationTypeFilter()).toBeUndefined();
+
+    // when
+    searchStore.publicationTypeArticle = true;
+    searchStore.publicationTypeBook = true;
+    searchStore.publicationTypeBookPart = true;
+    searchStore.publicationTypeConferencePaper = true;
+    searchStore.publicationTypePeriodicalPart = true;
+    searchStore.publicationTypeProceedings = true;
+    searchStore.publicationTypeResearchReport = true;
+    searchStore.publicationTypeThesis = true;
+    searchStore.publicationTypeWorkingPaper = true;
+    // then
+    expect((wrapper.vm as any).buildPublicationTypeFilter()).toBe(
+      "ARTICLE,BOOK,BOOK_PART,CONFERENCE_PAPER,PERIODICAL_PART,PROCEEDINGS,RESEARCH_REPORT,THESIS,WORKING_PAPER"
+    );
+  });
+
+  it("testAccessStateFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+    const searchStore = useSearchStore();
+    // when + then
+    searchStore.accessStateOpen = false;
+    searchStore.accessStateRestricted = false;
+    searchStore.accessStateClosed = false;
+    expect((wrapper.vm as any).buildAccessStateFilter()).toBeUndefined();
+
+    // when
+    searchStore.accessStateOpen = true;
+    searchStore.accessStateRestricted = true;
+    searchStore.accessStateClosed = true;
+    expect((wrapper.vm as any).buildAccessStateFilter()).toBe(
+      "OPEN,RESTRICTED,CLOSED"
+    );
+  });
+
+  it("testTempValFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+    const searchStore = useSearchStore();
+    // when + then
+    searchStore.temporalValidityFilterFuture = false;
+    searchStore.temporalValidityFilterPast = false;
+    searchStore.temporalValidityFilterPresent = false;
+    expect((wrapper.vm as any).buildTempValFilter()).toBeUndefined();
+
+    // when
+    searchStore.temporalValidityFilterFuture = true;
+    searchStore.temporalValidityFilterPast = true;
+    searchStore.temporalValidityFilterPresent = true;
+    expect((wrapper.vm as any).buildTempValFilter()).toBe(
+      "FUTURE,PAST,PRESENT"
+    );
+  });
+
+  it("testDateAtFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+
+    const searchStore = useSearchStore();
+    // when + then
+    searchStore.temporalEventStartDateFilter = false;
+    expect((wrapper.vm as any).buildStartDateAtFilter()).toBeUndefined();
+    searchStore.temporalEventStartDateFilter = true;
+    searchStore.temporalEventInput = "";
+    expect((wrapper.vm as any).buildStartDateAtFilter()).toBeUndefined();
+    searchStore.temporalEventInput = "2022-01-01";
+    expect((wrapper.vm as any).buildStartDateAtFilter()).toBe("2022-01-01");
+
+    // when + then
+    searchStore.temporalEventEndDateFilter = false;
+    expect((wrapper.vm as any).buildEndDateAtFilter()).toBeUndefined();
+    searchStore.temporalEventEndDateFilter = true;
+    searchStore.temporalEventInput = "";
+    expect((wrapper.vm as any).buildEndDateAtFilter()).toBeUndefined();
+    searchStore.temporalEventInput = "2022-01-01";
+    expect((wrapper.vm as any).buildEndDateAtFilter()).toBe("2022-01-01");
+  });
+
+  it("testFormalRuleFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+    const searchStore = useSearchStore();
+    // when + then
+    searchStore.formalRuleLicenceContract = false;
+    searchStore.formalRuleOpenContentLicence = false;
+    searchStore.formalRuleUserAgreement = false;
+    expect((wrapper.vm as any).buildFormalRuleFilter()).toBeUndefined();
+
+    // when
+    searchStore.formalRuleLicenceContract = true;
+    searchStore.formalRuleOpenContentLicence = true;
+    searchStore.formalRuleUserAgreement = true;
+    expect((wrapper.vm as any).buildFormalRuleFilter()).toBe(
+      "LICENCE_CONTRACT,OPEN_CONTENT_LICENCE,ZBW_USER_AGREEMENT"
+    );
+  });
+
+  it("testValidOnFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+    const searchStore = useSearchStore();
+    // when + then
+    searchStore.temporalValidOn = "";
+    expect((wrapper.vm as any).buildValidOnFilter()).toBeUndefined();
+
+    // when
+    searchStore.temporalValidOn = "2022-01-01";
+    expect((wrapper.vm as any).buildValidOnFilter()).toBe("2022-01-01");
+  });
+
+  it("testPaketSigelIdFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+
+    const searchStore = useSearchStore();
+    searchStore.availablePaketSigelIds = ["foo", "bar", "baz"];
+    searchStore.paketSigelIdIdx = [true, false, true];
+
+    // when + then
+    expect((wrapper.vm as any).buildPaketSigelIdFilter()).toBe("foo,baz");
+
+    searchStore.paketSigelIdIdx = [];
+    // when + then
+    expect((wrapper.vm as any).buildPaketSigelIdFilter()).toBeUndefined();
+  });
+
+  it("testZDBIdFilter", async () => {
+    // given
+    mockedApi.getList.mockReturnValue(
+      Promise.resolve({
+        itemArray: Array<ItemRest>({
+          metadata: {},
+          rights: {},
+        } as ItemRest),
+        totalPages: 25,
+      } as ItemInformation)
+    );
+    wrapper = shallowMount(ItemList, {
+      mocks: { api: mockedApi },
+      pinia: createTestingPinia({
+        stubActions: false,
+      }),
+    });
+
+    const searchStore = useSearchStore();
+    searchStore.availableZDBIds = ["foo", "bar", "baz"];
+    searchStore.zdbIdIdx = [true, false, true];
+
+    // when + then
+    expect((wrapper.vm as any).buildZDBIdFilter()).toBe("foo,baz");
+
+    searchStore.availableZDBIds = [];
+    // when + then
+    expect((wrapper.vm as any).buildPaketSigelIdFilter()).toBeUndefined();
+  });
 });
