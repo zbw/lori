000ef98ceee53569d2a99a666a25ed187edb0839
Christian Bay <christian.bay@posteo.net>
2023-07-31 15:27:05 +0200

    ci: try to find changed job files

--- scripts/pushJobImages.sh
+++ scripts/pushJobImages.sh
@@ -3,16 +3,14 @@
 ## Author   : Christian Bay
 ## About    : A small helper script that returns a list of all microservices
 ##              that have been changed in this branch.
+set -xe
 regex="job\/(\w*)\/\S*"
-
-# Production
-GITLAB_API_DATA=$(curl -s --header "PRIVATE-TOKEN:$GITLAB_API_ACCESS_TOKEN" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA")
-MR_BRANCH_LAST_COMMIT_SHA=$(echo "$GITLAB_API_DATA" | jq -r '.parent_ids | del(.[] | select(. == "'"$CI_COMMIT_BEFORE_SHA"'")) | .[-1]')
 
 # Test: Use a local commithash and comment the 'Production' section
 # MR_BRANCH_LAST_COMMIT_SHA=0686d557afb76933f18cc99105e4c1057f809f6e
 
-changedFiles=$(git diff-tree --no-commit-id --name-only -r "$MR_BRANCH_LAST_COMMIT_SHA")
+changedFiles=$(git diff-tree --no-commit-id --name-only -r "${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}".."${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}")
+echo "Changed files: $changedFiles"
 changedServices=()
 for f in $changedFiles    # unquoted in order to allow the glob to expand
 do
