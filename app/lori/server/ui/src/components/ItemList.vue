c15431e3122c8fcb1ddb5811654db907a1cf54ff
Christian Bay <christian.bay@posteo.net>
2024-07-11 16:41:17 +0200

    feat: make metadata column sortable

--- app/lori/server/ui/src/components/ItemList.vue
+++ app/lori/server/ui/src/components/ItemList.vue
@@ -63,7 +63,7 @@ export default defineComponent({
       {
         title: "Metadata-Id",
         align: "start",
-        sortable: false,
+        sortable: true,
         value: "metadataId",
       },
       {
