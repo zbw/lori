1f4a4558002e7f28a296d6eb109376350f728970
Christian Bay <christian.bay@posteo.net>
2024-09-16 16:44:15 +0200

    refactor: delete unused indices

---
+++ app/lori/server/src/main/resources/db/migration/V54__DROP_INDICES.sql
@@ -0,0 +1,8 @@
+DROP INDEX trgm_collection_idx;
+DROP INDEX trgm_community_idx;
+DROP INDEX trgm_sigel_idx;
+DROP INDEX trgm_title_idx;
+DROP INDEX trgm_zdb_idx;
+DROP INDEX item_isbn_idx;
+DROP INDEX item_issn_idx;
+DROP INDEX item_doi_idx;
