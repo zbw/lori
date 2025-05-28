615f7adc838f405d730581b23ad8772e254a4387
Christian Bay <christian.bay@posteo.net>
2025-05-28 09:26:17 +0200

    fix: whenever bookmark/templates list are queried use 500 as limit

--- app/lori/server/src/main/kotlin/de/zbw/api/lori/server/route/TemplateRoutes.kt
+++ app/lori/server/src/main/kotlin/de/zbw/api/lori/server/route/TemplateRoutes.kt
@@ -590,7 +590,7 @@ fun Routing.templateRoutes(
                             call.request.queryParameters["excludes"]?.let {
                                 it.split(",".toRegex())
                             }
-                        if (limit < 1 || limit > 501) {
+                        if (limit < 1 || limit > 500) {
                             span.setStatus(
                                 StatusCode.ERROR,
                                 "BadRequest: Limit parameter is expected to be between 1 and 200.",
