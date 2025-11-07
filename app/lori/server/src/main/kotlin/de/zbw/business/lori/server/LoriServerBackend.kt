7b66b8a290d586f4a49e05dead84d4bc659af8e1
Christian Bay <christian.bay@posteo.net>
2025-11-07 17:02:09 +0100

    test: fix for test

--- app/lori/server/src/main/kotlin/de/zbw/business/lori/server/LoriServerBackend.kt
+++ app/lori/server/src/main/kotlin/de/zbw/business/lori/server/LoriServerBackend.kt
@@ -82,7 +82,7 @@ class LoriServerBackend(
         handles: List<String>,
         createdBy: String,
     ): String {
-        val pkRight = dbConnector.rightDB.insertRight(right.copy(isTemplate = false, templateName = null))
+        val pkRight = dbConnector.rightDB.insertRight(right)
         dbConnector.itemDB.upsertItemBatch(
             createdBy = createdBy,
             itemIds =
